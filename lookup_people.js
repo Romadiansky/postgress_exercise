24aceb80576e1e8447de12ddfa4acd3ea18a9fd3
Diana Romanovsky <letsshitrainbows@gmail.com>
2018-10-16 21:08:11 +0000

    created a js query using process.argv to search through my db using pg and psql

---
+++ lookup_people.js
@@ -0,0 +1,29 @@
+const pg = require("pg");
+const settings = require("./settings"); // settings.json
+
+const client = new pg.Client({
+  user     : settings.user,
+  password : settings.password,
+  database : settings.database,
+  host     : settings.hostname,
+  port     : settings.port,
+  ssl      : settings.ssl
+});
+
+const searchQ = process.argv[2];
+
+client.connect((err) => {
+  if (err) {
+    return console.error("Connection Error", err);
+  }
+  client.query(`SELECT * FROM famous_people WHERE first_name
+    LIKE $1::text OR last_name LIKE $1::text`, [searchQ], (err, result) => {
+    if (err) {
+      return console.error("error running query", err);
+    }
+    let people = result.rows;
+    people.forEach( (person) => {
+      console.log((people.indexOf(person)+1).toString(), ' ', person.first_name, ' ', person.last_name, ' ', person.birthdate)
+    });
+  });
+});
