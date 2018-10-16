8322a579587f673971dc7ecd405973007c7a4026
Diana Romanovsky <letsshitrainbows@gmail.com>
2018-10-16 21:59:09 +0000

    added an introductory sentence to log what exactly our tabe will be showing

--- lookup_people.js
+++ lookup_people.js
@@ -22,8 +22,10 @@ client.connect((err) => {
       return console.error("error running query", err);
     }
     let people = result.rows;
-    people.forEach( (person) => {
-      console.log((people.indexOf(person)+1).toString(), ' ', person.first_name, ' ', person.last_name, ' ', person.birthdate)
+    let n = people.length;
+    console.log("Found " + n + " person(s) with the first or last name containing " + searchQ);
+    people.forEach( (person, index) => {
+      console.log((index+1).toString(), ' ', person.first_name, ' ', person.last_name, ' ', person.birthdate)
     });
   });
 });
